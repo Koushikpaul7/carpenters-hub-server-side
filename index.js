b36ff145e9afdeae3e79b7891a803b7bb867c3ad
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-23 17:30:55 +0600

    user access

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ async function run(){
                 $set: user
             };
             const result= await userCollection.updateOne(filter,updateDoc,options);
-            res.send(result);
+            const token=json.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+            res.send({result,token});
         })
 
 
