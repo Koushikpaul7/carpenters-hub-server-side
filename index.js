969e7d475e71c692d17a48b0004ab0fb7975f60a
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-25 20:46:10 +0600

    payment

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 const query = require('express/lib/middleware/query');
 require('dotenv').config();
 const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 app.use(cors());
 app.use(express.json());
@@ -40,6 +42,7 @@ async function run(){
         const orderCollection= client.db('carpenter_hub').collection('orders');
         const userCollection= client.db('carpenter_hub').collection('users');
         const reviewCollection= client.db('carpenter_hub').collection('reviews');
+        const paymentCollection= client.db('carpenter_hub').collection('payments');
 
 
         const verifyAdmin=async(req,res,next)=>{
@@ -130,6 +133,34 @@ async function run(){
             
         })
 
+        // get payment
+        app.get('/order/:id',verifyJWT,async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+            const order= await orderCollection.findOne(query);
+            res.send(order);
+        })
+
+        //update order
+        app.patch('/order/:id',verifyJWT,async(req,res)=>{
+            const id=req.params.id;
+            const payment=req.body;
+            const filter={_id:ObjectId(id)};
+            const updateDoc={
+                $set:{
+                    paid:true,
+                    transactionId:payment.transactionId,
+
+                }
+            }
+
+            const result= await paymentCollection.insertOne(payment);
+            const updatedOrder=await orderCollection.updateOne(filter,updateDoc);
+            res.send(updateDoc)
+
+        })
+
+
         //all order
 
         app.get('/manageOrder', verifyJWT,verifyAdmin, async(req,res)=>{
@@ -194,7 +225,19 @@ async function run(){
             res.send({result,token});
         })
 
+        //payment intent
 
+        app.post('/create-payment-intent',verifyJWT,async(req,res)=>{
+            const product=req.body;
+            const price= product.price;
+            const amount=price*100;
+            const paymentIntent=await stripe.paymentIntents.create({
+                amount:amount,
+                currency:'usd',
+                payment_method_types:['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+        })
     }
     finally{}
 }
