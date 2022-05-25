87ec46960dbcffe4397b0a970a3115caad3a8633
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-26 00:44:33 +0600

    payment

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ async function run(){
                 payment_method_types:['card']
             });
             res.send({clientSecret: paymentIntent.client_secret})
-        });
+        })
     }
     finally{}
 }
